f0d3f72722ea76b68be0088376cfd944470ff1b1
jhluc <jhluc@uci.edu>
2024-01-22 23:48:36 -0800

    implemented primitive parallax

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
-var $ = jQuery.noConflict()
-$(window).scroll(function(){
-  var scroll = $(this).scrollTop()
-  $(".parallax").css({"background-position":"0px "+scroll/2+"px"})
-}) 
+// var $ = jQuery.noConflict()
+// $(window).scroll(function(){
+//   var scroll = $(this).scrollTop()
+//   $(".parallax").css({"background-position":"0px "+scroll/2+"px"})
+// }) 
+
+let pic1 = document.getElementById("firstimage");
+let pic2 = document.getElementById("secondimage");
+window.addEventListener('scroll', function() {
+    var value = window.scrollY;
+    pic1.style.top = value  * 0.5 + 'px';
+    pic2.style.top = value  * 0.2 + 'px';
+})
