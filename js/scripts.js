e1c0f3b254f20fd916c646d773125c336efd21ae
jhluc <jhluc@uci.edu>
2024-01-23 10:40:19 -0800

    somewhat

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,14 @@
 
 let pic1 = document.getElementById("firstimage");
 let pic2 = document.getElementById("secondimage");
+let pic3 = document.getElementById("thirdimage");
+// let t1 = document.getElementById("firsttab");
+// let t2 = document.getElementById("secondtab");
 window.addEventListener('scroll', function() {
     var value = window.scrollY;
-    pic1.style.top = value  * 0.5 + 'px';
-    pic2.style.top = value  * 0.2 + 'px';
+    pic1.style.top = value  * 0.8 + 'px';
+    pic2.style.top = value  * 0.5 + 'px';
+    pic3.style.top = value * 0.9 + 'px';
+    // // t1.style.top = value * 0.8 + 'px';
+    // t2.style.top = value * 0.9 + 'px';
 })
